cae15759ae629378fdbd25366f856954b108618b
Patrick Lehmann <patlehmann1@gmail.com>
2018-08-03 13:58:57 -0400

    got the server to render html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends =   {
         "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "gender": "Male",
         "scores":[
             5,
             1,
@@ -13,7 +13,9 @@ var friends =   {
             4,
             1
           ]
-      },
+      };
 
       module.exports = friends;
+
+      
       
