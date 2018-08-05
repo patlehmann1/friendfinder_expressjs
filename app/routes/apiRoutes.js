44e84e70aae29e323a20988884b66abbcf2567d7
Patrick Lehmann <patlehmann1@gmail.com>
2018-08-05 11:45:23 -0400

    deleted comments

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -20,27 +20,20 @@ module.exports = function(app) {
 			friendDifference: Infinity
 		};
 
-		// Here we take the result of the user's survey POST and parse it.
 		var userData 	= req.body;
 		var userScores 	= userData.scores;
 
 		var totalDifference;
 
-		// Loop through all the friend possibilities in the database. 
 		for  (var i=0; i< friends.length; i++) {
 
 			console.log(friends[i].name);
 			totalDifference = 0;
 
-			// Loop through all the scores of each friend
 			for (var j=0; j< friends[i].scores.length; j++){
-				// We calculate the difference between the scores and sum them into the totalDifference
 				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-
-				// If the sum of differences is less then the differences of the current "best match"
 				if (totalDifference <= bestMatch.friendDifference){
 
-					// Reset the bestMatch to be the new friend. 
 					bestMatch.name = friends[i].name;
 					bestMatch.age = friends[i].age;
 					bestMatch.friendDifference = totalDifference;
@@ -49,11 +42,8 @@ module.exports = function(app) {
 			}
 		}
 
-		// Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-		// the database will always return that the user is the user's best friend).
 		friends.push(userData);
 
-		// Return a JSON with the user's bestMatch. This will be used by the HTML in the next page. 
 		res.json(bestMatch);
 
 	});   
